11cad692ca23d462e7c5f04a9648dcf15b2948d8
James <80452203+JeremiahJhon@users.noreply.github.com>
2023-05-30 23:42:53 +0700

    policy

--- AITS MIS/Controllers/MarineDebrisController.cs
+++ AITS MIS/Controllers/MarineDebrisController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics.PerformanceData;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -293,5 +294,86 @@ namespace UCOnline.Controllers
 
             return JsonConvert.SerializeObject(result);
         }
+
+        [HttpGet]
+        public String getPolicyData(int country, int area)
+        {
+            string filter = "";
+            if (country > 0)
+            {
+                filter += " and Country_Id = " + country;
+            }
+            if (area > 0)
+            {
+                if (filter != "") filter += " and ";
+
+                filter += "Area_Id = " + area;
+            }
+            DataSet data = new DataSet();
+            ServerBase countryPolicy = new ServerBase("CountryPolicy");
+            countryPolicy.SelectFilter("(Area_ID > 0 and Area_ID is not null) and (Country_ID > 0 and Country_ID is not null)" + filter);
+            countryPolicy.SelectOrder("ID", Web.Framework.Enums.EnumOrder.DESCENDING);
+            DataTable countryPolicyData = countryPolicy.SelectQuery();
+            ServerBase country_ = new ServerBase("Country");
+            country_.SelectFilter("SubRegion_ID is not null and SubRegion_ID <> '' and SubRegion_ID in (select ID from SubRegion where Region_id = 3)"); // 3 = Asia
+            DataTable countryData = country_.SelectQuery();
+            ServerBase countryPolicyType = new ServerBase("CountryPolicy_Type");
+            DataTable countryPolicyTypeData = countryPolicyType.SelectQuery();
+            ServerBase countryPolicyArea = new ServerBase("CountryPolicy_Area");
+            DataTable countryPolicyAreaData = countryPolicyArea.SelectQuery();
+            ServerBase wasteCategory = new ServerBase("WasteCategory");
+            DataTable wasteCategoryData = wasteCategory.SelectQuery();
+
+            DataTable dtResult = new DataTable();
+            dtResult.Columns.Add("ID", typeof(int));
+            dtResult.Columns.Add("code_number", typeof(string));
+            dtResult.Columns.Add("Title", typeof(string));
+            dtResult.Columns.Add("Year", typeof(string));
+            dtResult.Columns.Add("Description", typeof(string));
+            dtResult.Columns.Add("Country_ID", typeof(string));
+            dtResult.Columns.Add("Country", typeof(string));
+            dtResult.Columns.Add("WasteCategory", typeof(string));
+            dtResult.Columns.Add("PolicyType", typeof(string));
+            dtResult.Columns.Add("Area_ID", typeof(int));
+            dtResult.Columns.Add("Area", typeof(string));
+            dtResult.Columns.Add("Link", typeof(string));
+
+            if (countryPolicyData.Rows.Count == 0)
+            {
+                return JsonConvert.SerializeObject(dtResult);
+            }
+
+            var JoinResult = from a in countryPolicyData.AsEnumerable()
+                             join b in countryData.AsEnumerable()
+                             on a.Field<String>("Country_ID").ToString() equals b.Field<int>("ID").ToString()
+                             join c in countryPolicyTypeData.AsEnumerable()
+                             on a.Field<int?>("Type_ID").ToString() equals c.Field<int?>("ID").ToString() into bc
+                             from ac in bc.DefaultIfEmpty()
+                             join d in countryPolicyAreaData.AsEnumerable()
+                             on a.Field<int?>("Area_ID").ToString() equals d.Field<int?>("ID").ToString() into bd
+                             from ad in bd.DefaultIfEmpty()
+                             join e in wasteCategoryData.AsEnumerable()
+                             on a.Field<int?>("WasteCategory_ID").ToString() equals e.Field<int?>("ID").ToString() into be
+                             from ae in be.DefaultIfEmpty()
+                             select dtResult.LoadDataRow(new object[]
+                             {
+                                a.Field<int>("ID"),
+                                a.Field<string>("code_number"),
+                                a.Field<string>("Legal"),
+                                a.Field<string>("Year"),
+                                a.Field<string>("Description"),
+                                a.Field<string>("Country_ID"),
+                                b.Field<string>("Name"),
+                                ae == null ? "" : ae.Field<string>("Name"),
+                                ac == null ? "" : ac.Field<string>("Name"),
+                                a.Field<int>("Area_ID"),
+                                ad == null ? "" : ad.Field<string>("Name"),
+                                a.Field<string>("Link"),
+                             }, false);
+            JoinResult.CopyToDataTable();
+
+            return JsonConvert.SerializeObject(dtResult);
+        }
+
     }
 }
